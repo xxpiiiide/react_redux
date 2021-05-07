77280c3aa058bbad2d2fc37d45f151b73406e307
이 필 제 <xxpiiiide@gmail.com>
2021-05-07 17:30:10 +0900

    1.4 Recap Refactor

--- src/index.js
+++ src/index.js
@@ -4,14 +4,29 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+number.innerText = 0;
+
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 // reducer는 data를 modify 하는 함수이다.
 const countModifier = (count = 0, action) => {
-	if (action.type === 'ADD') {
-		return count + 1;
-	} else if (action.type === 'MINUS') {
-		return count - 1;
-	} else {
-		return count;
+	// if (action.type === 'ADD') {
+	// 	return count + 1;
+	// } else if (action.type === 'MINUS') {
+	// 	return count - 1;
+	// } else {
+	// 	return count;
+	// }
+	// if와 else 말고 switch를 써보도록 해볼 것
+
+	switch (action.type) {
+		case ADD:
+			return count + 1;
+		case MINUS:
+			return count - 1;
+		default:
+			return count;
 	}
 };
 
@@ -24,11 +39,11 @@ const onChange = () => {
 countStore.subscribe(onChange); // subscribe는 store 안에 있는 변화들을 알 수 있게 해줍니다.
 
 const handleAdd = () => {
-	countStore.dispatch({ type: 'ADD' });
+	countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-	countStore.dispatch({ type: 'MINUS' });
+	countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener('click', handleAdd);
