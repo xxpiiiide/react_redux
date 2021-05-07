26d93930c5eaa1dbfad78f918719e993cfb4f446
이 필 제 <xxpiiiide@gmail.com>
2021-05-07 17:10:30 +0900

    1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -6,23 +6,30 @@ const number = document.querySelector('span');
 
 // reducer는 data를 modify 하는 함수이다.
 const countModifier = (count = 0, action) => {
-	console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  }
-	return count;
+	if (action.type === 'ADD') {
+		return count + 1;
+	} else if (action.type === 'MINUS') {
+		return count - 1;
+	} else {
+		return count;
+	}
 };
 
 const countStore = createStore(countModifier); // createeStore는 reducer를 받는 함수여야한다.
 
-countStore.dispatch({type: "ADD"}) // store의 dispatch를 이용하여 action을 보낼 수 있다. 파라미터는 객체여야한다.
-countStore.dispatch({type: "ADD"})
-countStore.dispatch({type: "ADD"})
-countStore.dispatch({type: "ADD"})
-countStore.dispatch({type: "ADD"})
-countStore.dispatch({type: "MINUS"})
+const onChange = () => {
+	number.innerText = countStore.getState();
+};
 
+countStore.subscribe(onChange); // subscribe는 store 안에 있는 변화들을 알 수 있게 해줍니다.
 
-console.log(countStore.getState())
+const handleAdd = () => {
+	countStore.dispatch({ type: 'ADD' });
+};
+
+const handleMinus = () => {
+	countStore.dispatch({ type: 'MINUS' });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
