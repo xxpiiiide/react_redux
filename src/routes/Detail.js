31a02d06afc46396213711ed4a464ea3909aac93
이 필 제 <xxpiiiide@gmail.com>
2021-05-11 17:10:25 +0900

    3.5 Detail Screen

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+// import { useParams } from 'react-router-dom';
 
-function Detail() {
+function Detail({ toDo, onBtnClick }) {
+	// const id = useParams(); // useParams를 이용하는 방법도 있다.
+	// console.log('id >>>', id);
+
 	return (
-		<>
-			<h1>To Do</h1>
-			<form>
-				<input type="text" />
-			</form>
+		<> 
+			<h1>{toDo?.text}</h1> 
+			<h5>Created at: {toDo?.id}</h5>
 		</>
 	);
 }
 
-export default Detail;
+function mapStateToProps(state, ownProps) {
+	// console.log(ownProps);
+	const {
+		match: {
+			params: { id },
+		},
+	} = ownProps;
+	return { toDo: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
