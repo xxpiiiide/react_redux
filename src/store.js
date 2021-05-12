3f3f74c6f3145a98f9f264377bfed61dcba12bf0
이 필 제 <xxpiiiide@gmail.com>
2021-05-12 12:00:48 +0900

    4.0 Redux Toolkit

--- src/store.js
+++ src/store.js
@@ -1,5 +1,11 @@
 import { createStore } from 'redux';
 
+
+// 리덕스의 경우 action과 많은 actionCreator를 써야하는 단점들이 있다.
+// switch의 경우 case, default 등 써야할 부분이 많다.
+// 그래서 사람들이 생각할 때 많은 코드를 쳐야한다고 생각한다 >>> 그래서 Redux toolkit(적은량의 코드로 같은 기능을 하게 도와줌)을 사용하면 좋다.
+
+
 const ADD = 'ADD';
 const DELETE = 'DELETE';
 
